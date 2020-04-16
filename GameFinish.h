751297a6734da0c4465148a6802f6e752ad1c396
Pham Vu Minh <vuminhpham2001@gmail.com>
2020-04-16 21:22:01 +0700

    second commit

---
+++ GameFinish.h
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<SDL.h>
+using namespace std;
+
+void youWin(SDL_Renderer* renderer, SDL_Event event, int scale, int wScale, int tailLength){
+
+	SDL_Color Red = { 255, 0, 0 };
+	SDL_Color White = { 255, 255, 255 };
+	SDL_Color Yellow = { 255, 255, 0 };
+
+	TTF_Font* font = TTF_OpenFont((char*)"arial.ttf", 10);
+	if (font == NULL){
+		cout << "Font loading error" << endl;
+		return;
+	}
+
+	SDL_Surface* score = TTF_RenderText_Solid(font, (string("Score: ") + to_string(tailLength * 10)).c_str(),White);
+	SDL_Surface* gameover = TTF_RenderText_Solid(font, "You win!", Yellow);
+	SDL_Surface* retry = TTF_RenderText_Solid(font, "Press Enter to play again", White);
+	SDL_Texture* gameoverMessage = SDL_CreateTextureFromSurface(renderer, gameover);
+	SDL_Texture* retryMessage = SDL_CreateTextureFromSurface(renderer, retry);
+	SDL_Texture* scoreMessage = SDL_CreateTextureFromSurface(renderer, score);
+	SDL_Rect gameoverRect;
+	SDL_Rect retryRect;
+	SDL_Rect scoreRect;
+
+	scoreRect.h = 50;
+	scoreRect.w = 100;
+	scoreRect.x = ((scale*wScale)/2) - (scoreRect.w/2);
+	scoreRect.y = 0;
+	gameoverRect.h = 200;
+	gameoverRect.w = 300;
+	gameoverRect.x = ((scale*wScale)/2) - (gameoverRect.w/2);
+	gameoverRect.y = ((scale*wScale)/2) - (gameoverRect.h/2) - 50;
+	retryRect.h = 100;
+	retryRect.w = 300;
+	retryRect.x = ((scale*wScale)/2) - ((retryRect.w/2));
+	retryRect.y = (((scale*wScale)/2) - ((retryRect.h/2)) + 150);
+	SDL_RenderCopy(renderer, gameoverMessage, NULL, &gameoverRect);
+	SDL_RenderCopy(renderer, retryMessage, NULL, &retryRect);
+	SDL_RenderCopy(renderer, scoreMessage, NULL, &scoreRect);
+
+	TTF_CloseFont(font);
+
+	// Show victory screen while space has not been pressed
+	while (true){
+		SDL_RenderPresent(renderer);
+
+		if (SDL_PollEvent(&event)){
+
+			if (event.type == SDL_QUIT){
+				exit(0);
+			}
+
+			if (event.key.keysym.scancode == SDL_SCANCODE_RETURN){
+				return;
+			}
+		}
+	}
+}
+
+void gameOver(SDL_Renderer* renderer, SDL_Event event, int scale, int wScale, int tailLength){
+	SDL_Color Red = { 255, 0, 0 };
+	SDL_Color White = { 255, 255, 255 };
+
+	// Get the font used for displaying text
+	TTF_Font* font = TTF_OpenFont((char*)"arial.ttf", 10);
+	if (font == NULL){
+		cout << "Font loading error" << endl;
+		return;
+	}
+
+	SDL_Surface* score = TTF_RenderText_Solid(font, (string("Score: ") + to_string(tailLength * 10)).c_str(), White);
+	SDL_Surface* gameover = TTF_RenderText_Solid(font, "Game Over", Red);
+	SDL_Surface* retry = TTF_RenderText_Solid(font, "Press Enter to retry", White);
+	SDL_Texture* gameoverMessage = SDL_CreateTextureFromSurface(renderer, gameover);
+	SDL_Texture* retryMessage = SDL_CreateTextureFromSurface(renderer, retry);
+	SDL_Texture* scoreMessage = SDL_CreateTextureFromSurface(renderer, score);
+	SDL_Rect gameoverRect;
+	SDL_Rect retryRect;
+	SDL_Rect scoreRect;
+
+	scoreRect.h = 50;
+	scoreRect.w = 100;
+	scoreRect.x = ((scale*wScale)/2) - (scoreRect.w/2);
+	scoreRect.y = 0;
+	gameoverRect.h = 200;
+	gameoverRect.w = 300;
+	gameoverRect.x = ((scale*wScale)/2) - (gameoverRect.w/2);
+	gameoverRect.y = ((scale*wScale)/2) - (gameoverRect.h/2) - 50;
+	retryRect.h = 100;
+	retryRect.w = 300;
+	retryRect.x = ((scale*wScale)/2) - ((retryRect.w/2));
+	retryRect.y = (((scale*wScale)/2) - ((retryRect.h/2)) + 150);
+	SDL_RenderCopy(renderer, gameoverMessage, NULL, &gameoverRect);
+	SDL_RenderCopy(renderer, retryMessage, NULL, &retryRect);
+	SDL_RenderCopy(renderer, scoreMessage, NULL, &scoreRect);
+
+	TTF_CloseFont(font);
+
+	// Show game over screen while space has not been pressed
+	while (true){
+		SDL_RenderPresent(renderer);
+
+		if (SDL_PollEvent(&event)){
+			if (event.type == SDL_QUIT){
+				exit(0);
+			}
+			if (event.key.keysym.scancode == SDL_SCANCODE_RETURN){
+				return;
+			}
+		}
+	}
+}
---
+++ PlayerAndFood.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+//Create player
+void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int scale, vector<int> tailX, vector<int> tailY, int tailLength){
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	player.w = scale;
+	player.h = scale;
+
+	// Render tail blocks
+	for (int i = 0; i < tailLength; i++){
+		player.x = tailX[i];
+		player.y = tailY[i];
+		SDL_RenderFillRect(renderer, &player);
+	}
+
+	player.x = x;
+	player.y = y;
+
+	SDL_RenderFillRect(renderer, &player);
+}
+//Render food
+void renderFood(SDL_Renderer* renderer, SDL_Rect food){
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderFillRect(renderer, &food);
+}
+
+// Spawn food and make sure it dont spawn on the body of player
+pair<int, int> getFood(vector<int> tailX, vector<int> tailY, int playerX, int playerY, int scale, int wScale, int tailLength){
+    bool valid = false;
+	int x = 0;
+	int y = 0;
+	srand(time(0));
+	x = scale * (rand() % wScale);
+	y = scale * (rand() % wScale);
+	valid = true;
+
+	// Check all tail blocks and player block
+	for (int i = 0; i < tailLength; i++) {
+		if ((x == tailX[i] && y == tailY[i]) || (x == playerX && y == playerY)){
+			valid = false;
+		}
+	}
+	if (!valid) {
+		pair<int, int> foodLoc;
+		foodLoc = make_pair(-100, -100);
+		return foodLoc;
+	}
+
+	pair<int, int> foodLoc;
+	foodLoc = make_pair(x, y);
+	return foodLoc;
+}
+
